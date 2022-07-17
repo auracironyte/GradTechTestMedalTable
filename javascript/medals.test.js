9b77d5ece5c82e5f7691a5215fea2d43482d3b84
Aura <aura.cironyte@gmail.com>
2022-07-18 00:36:40 +0100

    commit

--- javascript/medals.test.js
+++ javascript/medals.test.js
@@ -1,7 +1,71 @@
+/**
+ * Parse the medal data to produce a medaltable
+ * The winner gets 3 points, second place 2 points and third place 1 point
+ */
+
+const countriesPoints = [
+    {
+        name: "Italy",
+        totalPoints: 0,
+    },
+    {
+        name: "France",
+        totalPoints: 0,
+    },
+    {
+        name: "ROC",
+        totalPoints: 0,
+    },
+    {
+        name: "USA",
+        totalPoints: 0,
+    },
+    {
+        name: "Qatar",
+        totalPoints: 0,
+    },
+    {
+        name: "China",
+        totalPoints: 0,
+    },
+    {
+        name: "Germany",
+        totalPoints: 0,
+    },
+    {
+        name: "Brazil",
+        totalPoints: 0,
+    },
+    {
+        name: "Belarus",
+        totalPoints: 0,
+    },
+]
+
+const podiumPointMap = {
+    "1": 3,
+    "2": 2,
+    "3": 1,
+}
+
 function createMedalTable(medals) {
-    // Parse the medal data to produce a medaltable
-    // The winner gets 3 points, second place 2 points and third place 1 point
-    return
+    medals.forEach(medalByPodium => {
+        medalByPodium.podium.forEach(podiumCountry => {
+            // split podium by county
+            const podiumCountryMap = podiumCountry.split(".");
+            const podium = podiumCountryMap[0];
+            const country = podiumCountryMap[1];
+            // find object in array and replace/change object totalPoints if founded
+            const foundIndex = countriesPoints.findIndex(obj => obj.name === country);
+            countriesPoints[foundIndex].totalPoints += podiumPointMap[podium];
+        });
+    });
+
+    let medalTableResult = {};
+    countriesPoints.forEach(({name, totalPoints}) =>
+        Object.assign(medalTableResult, {[name]: totalPoints}));
+
+    return medalTableResult;
 }
 
 describe("Medal Table Generator", () => {
@@ -9,9 +73,9 @@ describe("Medal Table Generator", () => {
     it("creates correct data structure ", () => {
         // Input data
         const medals = [{
-                sport: "cycling",
-                podium: ["1.China", "2.Germany", "3.ROC"]
-            },
+            sport: "cycling",
+            podium: ["1.China", "2.Germany", "3.ROC"]
+        },
             {
                 sport: "fencing",
                 podium: ["1.ROC", "2.France", "3.Italy"]
